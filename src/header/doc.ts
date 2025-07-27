394e50b89307896adfd96c760f36a7cf9aec4b7c
git <client@funxdata.com>
2025-07-27 12:39:25 +0800

    init tempalte

---
+++ src/header/doc.ts
@@ -0,0 +1,26 @@
+// 文档头
+const doc_header_html = `
+        <header class="vg-headers-doc row align-center" >
+        <div class="containers">
+        <div class="row align-center">
+        <a class="logo" data-url="/" fxtag="logo">
+        <img src="https://book.funxdata.com/public/img/webmanage/logo.png">
+        <p class="name">GUI</p>
+        </a>
+        <div class="flex-auto row align-center justify-end">
+        <div class="nav justify-content-end" >
+            <a class="nav-li {{navItemType}} select" href="/">Doc</a>
+            <a class="nav-li {{navItemType}}" href="/icons">Icons</a>
+        </div>
+        <div class="vg-switch ml-16 {{switchType}}" fxtag="switch">
+        <div class="vg-switch-core"></div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </header>
+
+`
+export const doc_header = ()=>{
+  
+}

5ea1e58388b3600f01f5a27c82d77d0fd1ce869f
git <client@funxdata.com>
2025-08-09 20:19:22 +0800

    fix pagination

--- comps/pagination/index.ts
+++ comps/pagination/index.ts
@@ -1,12 +1,15 @@
 export const pagination:string = `
 <p class="number">
-    共<span id="pagination_total"><%=it.paginationtpl.total%></span>个
+    共<span id="pagination_total"><%=it.total%></span>个
     <div class="vg-paginations-pages pcol-8">
-    <button class="vg-paginations-btn" id="pagination_front">
+
+    <button class="vg-paginations-btn  
+    <%= it.amount === 1||it.num === 1 ? "disabled" : "" %>
+    " id="pagination_front">
         <i class="vg-icon ic-left"></i>
     </button>
     <input type="number" id="pagination_input" type="number" value="<%=it.num%>">
-    <button class="vg-paginations-btn" id="pagination_behind">
+    <button class="vg-paginations-btn  <%= it.amount === 1 ? "disabled" : "" %>" id="pagination_behind">
         <i class="vg-icon ic-right"></i>
     </button>
     </div>
